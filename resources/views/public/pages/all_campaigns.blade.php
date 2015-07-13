b8d73b23e808d0f42af26c773e1c3d0fc56a0f03
Eskimo <eskimo@Eskimos-MacBook.local>
2015-07-13 21:02:06 +0100

    Finished authentication and logout

--- resources/views/public/pages/all_campaigns.blade.php
+++ resources/views/public/pages/all_campaigns.blade.php
@@ -20,7 +20,8 @@
 
 					<div class="top-btn-container pull-right">
 						<a class="btn btn-sm btn-primary" href="{{ url() }}/create"><i class="glyphicon glyphicon-plus"></i> Add Campaign</a>
-						<a class="btn btn-sm btn-danger" href="{{ url() }}/logout"><i class="glyphicon glyphicon-log-out"></i> Logout</a>
+						<a class="btn btn-sm btn-danger" href="{{ url('/auth/logout') }}"><i class="glyphicon glyphicon-log-out"></i> Logout</a>
+				</div>
 					</div>
 
 			</div>
