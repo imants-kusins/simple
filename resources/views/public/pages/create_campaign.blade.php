31f59f996f9e62a0fa757e99a19e5ffb91ce157f
Eskimo <eskimo@Eskimos-MacBook.home>
2015-07-13 15:18:26 +0100

    123

--- resources/views/public/pages/create_campaign.blade.php
+++ resources/views/public/pages/create_campaign.blade.php
@@ -16,13 +16,36 @@
 
 		</div>
 
-		<div class="row col-md-12">
+
+
+	<div class="campaign-create-form-container">
 			
 		<form method="POST">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+			<!-- form content -->
+
+				<div class="row col-md-8">
+					<div class="form-group">
+						<label>Campaign Name:</label>
+						<input type="text" name="campaign_name" value="" class="form-control" />
+					</div>
+				</div>
+
+				
+				<div class="row col-md-8">
+					<div class="form-group">
+						<label>Inbox Phone Number:</label>
+						<input type="text" name="campaign_phone_number" value="" class="form-control" />
+					</div>
+				</div>
+
+				
+
+			<!-- // -->
+
 		</form>
 
-		</div>
 
 	</div>
 
