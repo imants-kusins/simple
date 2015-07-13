4190b87442b9a0bdd871828574d3221de5cc0005
Eskimo <eskimo@Eskimos-MacBook.home>
2015-07-13 14:58:39 +0100

    Moved content in the sideba

--- resources/views/public/pages/all_campaigns.blade.php
+++ resources/views/public/pages/all_campaigns.blade.php
@@ -3,24 +3,7 @@
 @section('content')
 
 
-<div id="wrapper">
 
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <!-- <li class="sidebar-brand">
-                    <a href="#">
-                        Start Bootstrap
-                    </a>
-                </li> -->
-                <li>
-                    <a href="#">Campaign</a>
-                </li>
-                <li>
-                    <a href="#">Campaign</a>
-                </li>
-            </ul>
-        </div>
         <!-- /#sidebar-wrapper -->
 
         <!-- Page Content -->
